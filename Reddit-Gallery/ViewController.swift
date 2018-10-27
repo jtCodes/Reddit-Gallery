a1e67f6f8420366d35c255638b165fdfdabd0d6d
jtCodes <jt234wq@gmail.com>
2018-10-27 16:55:59 -0400

    first

---
+++ Reddit-Gallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reddit-Gallery
+//
+//  Created by J Tan on 10/27/18.
+//  Copyright © 2018 J Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
