b5f73e97f63c4c859c702bd9828bd9fab27fa738
jtCodes <jt234wq@gmail.com>
2018-10-27 21:54:40 -0400

    json decoding working

---
+++ Reddit-Gallery/SubRedditDecode.swift
@@ -0,0 +1,30 @@
+//
+//  SubRedditDecode.swift
+//  Reddit-Gallery
+//
+//  Created by J Tan on 10/27/18.
+//  Copyright © 2018 J Tan. All rights reserved.
+//
+
+import Foundation
+
+struct SubRedditDecode: Decodable {
+    struct Root: Decodable {
+        let data: RootData
+    }
+    
+    struct RootData: Decodable {
+        let children: [Child]
+    }
+    
+    struct Child: Decodable {
+        let data: Data
+    }
+    
+    struct Data: Decodable {
+        let title: String
+        let thumbnail: String
+        let url: String
+        let is_video: Bool
+    }
+}
--- Reddit-Gallery/ViewController.swift
+++ Reddit-Gallery/ViewController.swift
@@ -7,14 +7,22 @@
 //
 
 import UIKit
+import Alamofire
+
+let reqUrl = "https://www.reddit.com/r/videos/new.json?"
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        Alamofire.request(reqUrl).responseJSON { response in
+            if let data = response.data {
+                if let decodedSubRedditData = try? JSONDecoder().decode(SubRedditDecode.Root.self,from:  data) {
+                    print(decodedSubRedditData.data.children[0])
+                }
+            }
+        }
     }
-
-
 }
 
